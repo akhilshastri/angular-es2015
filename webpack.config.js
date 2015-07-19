c8a6ac369f608fec47693c8e2a5bc9375d8388fa
bhannat2012 <bhannat2012@gmail.com>
2015-07-19 19:40:55 +0530

    First check-in

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,15 @@
-/**
- * Created by Akhil on 19-07-2015.
- */
+var path = require('path');
+module.exports = {
+    entry: './src/es6/main.js',
+    output: {
+        path: __dirname,
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+           /* { test: path.join(__dirname, 'src/es6'), loader: 'babel-loader' }*/
+            { test:/\.js$/, loader: 'babel-loader' }
+
+        ]
+    }
+};
